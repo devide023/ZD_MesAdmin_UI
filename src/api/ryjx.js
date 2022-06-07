ee59b1d8e8931e3d2339d886589eea65d6007720
devide023 <devide@126.com>
2022-06-08 07:59:57 +0800

    1

--- src/api/ryjx.js
+++ src/api/ryjx.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 export default{
     ryjx:(data)=>{
         return request({
-            baseURL:'http://172.16.5.7:8033/api',
+            baseURL:'http://172.16.201.125:8033/api',
             url: '/User/GetListPerformanceByUserCode',
             method: 'post',
             data: data
