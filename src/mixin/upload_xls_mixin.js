0ab3ed87b9d7813e9e46136a1e632f7b98a740a8
devide023 <devide@126.com>
2022-06-25 21:59:32 +0800

    1

--- src/mixin/upload_xls_mixin.js
+++ src/mixin/upload_xls_mixin.js
@@ -9,7 +9,16 @@ export const upload_xls_mixin={
             if (!isLt5M) {
                 this.$message.error("上传文件大小不能超过 5MB!");
             }
-            return isxls >= 0 && isLt5M;
+            let isok = isxls >= 0 && isLt5M;
+            if(isok){
+                this.$loading({
+                    lock: true,
+                    text: "正在上传文件",
+                    spinner: "el-icon-loading",
+                    background: "rgba(0, 0, 0, 0.7)",
+                });
+            }
+            return isok;
         },
     },
 }
